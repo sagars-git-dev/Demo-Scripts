d12d6989dd2b0e4303cb142c5229df56714cda03
Ubuntu <ubuntu@ip-172-31-21-134.eu-west-1.compute.internal>
2025-03-01 10:24:52 +0000

    add 3 logs script

---
+++ 3_logs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+hero="rancho"
+villan="virus"
+
+echo " 3 idiots hero is $hero "
+echo " 3 idiots villan is $villan"
+
+echo " current user $USER"
+
+read -p " your full name ...?" fullname
+
+echo " full name is $fullname go"
+
+# ./3_idiots.sh raju farhan rancho 
+
+echo " movie name : $0"
+echo " first name : $1"
+echo "second name : $2"
+echo "third name :  $3"
+
+echo " so all are $@:wq
+"
+
+
+
+
+
+
+
